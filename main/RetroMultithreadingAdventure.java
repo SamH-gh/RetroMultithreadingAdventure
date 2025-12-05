f0ce72cac520b1350ee34229039c7b82937cfa0e
SamH-gh <samuel.w.hay93@gmail.com>
2025-12-05 08:10:21 -0600

    changed character names and items to fit a carnival theme.

--- main/RetroMultithreadingAdventure.java
+++ main/RetroMultithreadingAdventure.java
@@ -3,22 +3,29 @@ import java.util.Scanner;
 import playable.GameCharacter;
 
 public class RetroMultithreadingAdventure {
+
+    // Entry point for the Retro Multithreading Adventure game
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Haunted Carnival!");
+
+        // Main game loop for multiple encounters
         for (int i = 0; i < 2; i++) {
             if (GameCharacter.partyAlive == false) {
                 System.out.println("The party has been defeated. Game over.");
                 break;
             }
+
             System.out.println("The party approaches an undead clown telling jokes...");
             Clown clown = new Clown();
             clown.tellJoke();
             System.out.println("Was that joke funny? ('yes' or 'no') If not, prepare to fight!");
             String playerResponse = scanner.nextLine();
             playerResponse = playerResponse.toLowerCase();
+            
             if (playerResponse.equals("no")) {
-                // Start the threaded encounter; PlayerThreadControl creates and manages the fight
-                PlayerThreadControl.main(args);
+                // Start the encounter
+                GameEngine.runEncounter();
             } else {
                 System.out.println("The party decides the joke was funny and spares the clown.");
             }
